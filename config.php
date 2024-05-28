42304687eeaff8b38ae1c4723c7a18fc8904b7f2
Mausam Giri <48901104+mausam-giri@users.noreply.github.com>
2024-05-28 15:08:41 +0530

    removed config.php

--- config.php
+++ config.php
@@ -1,10 +1,1 @@
-<?php
 
-
-const SMTP_HOST = 'greenviewsolar.in';
-const SMTP_PORT_SSL = 465; // ssl
-const SMTP_PORT_TLS = 587; // ssl
-const SMTP_USERNAME = 'contact@greenviewsolar.in';
-const SMTP_PASSWORD = '{vgc!tA{{Gq~';
-const SMTP_FROM = 'contact@greenviewsolar.in';
-const SMTP_FROM_NAME = "Enquiry Greenview";
